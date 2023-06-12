7c0a1b61b36ac7524a10fd3a356854cb77aab75b
Melody Choi <melodychoi@princeton.edu>
2023-06-12 13:31:14 -0400

    put hmm into separate file

--- filter_snps.py
+++ filter_snps.py
@@ -1,11 +1,9 @@
 #!/bin/python
 
 import sys
-import matplotlib.pyplot as plt
-import scipy
 import numpy as np
+import pickle
 import pandas as pd
-from hmmlearn import hmm
 from collections import defaultdict
 import gzip
 from multiprocessing import Pool, cpu_count
@@ -104,81 +102,7 @@ def regenotype(format, sample):
         new_sample = f"{gt}:{pl}:{dp}:{ref},{ad}"
     return new_sample
 
-# --------------- FOR CUSTOM EMISSION PROBABILITIES --------------- ###
-#
-# def emission_loh(DP, AD, lost, retained):
-#     global count
-#     emission = math.comb(DP[count], AD[count]) * pow(lost, AD[count]) * pow(retained, DP[count] - AD[count])
-#     count += 1
-#     print("loh computed")
-#     return emission
-#
-# def emission_norm(DP, AD):
-#     global count
-#     emission = math.comb(DP[count], AD[count]) * pow(0.5, AD[count]) * pow(0.5, DP[count] - AD[count])
-#     count += 1
-#     print("norm computed")
-#     return emission
-#
-# ----------------------------------------------------------------- ###
-
-def hmm_sequence(AD, DP, purity):
-    """
-    Uses Gaussian HMM to decode and return a sequence of hidden states (LOH/normal) of each heterozygous SNP.
-    """
-    # create 1-D matrix of observed states (BAF) with all values <1 (LAF)
-    baf_full = np.divide(AD, DP)
-    with np.nditer(baf_full, op_flags=['readwrite']) as it:
-        for x in it:
-            if x > 0.5:
-                x[...] = 1 - x
-    BAF = np.array(baf_full).reshape(-1, 1)
-    p = float(purity)
-
-    # set probability matrices, mean, covariance
-    lost = (1 - p)/(2 - p)
-    retained = (p)/(2 - p)
-    start_prob = np.array([0.5, 0.5])
-    trans_prob = np.array([[1 - 1/30000, 1/30000],
-                           [1/30000, 1 - 1/30000]])
-    mean = np.array([[(1 - p)/(2 - p)], [0.5]])
-    covariance = np.array([[0.2], [0.2]])
-
-    # create and initialize Gaussian HMM
-    model = hmm.GaussianHMM(n_components=2)
-    model.startprob_ = start_prob
-    model.transmat_ = trans_prob
-    model.means_ = mean
-    model.covars_ = covariance
-      
-    # decode using BAF matrix
-    logprob, decoded_states = model.decode(BAF, algorithm="viterbi")
-    state_labels = {0: "LOH", 1: "Normal"}
-    sequence = []
-
-    ### FOR WRITING OUT HMM SEQUENCE ONTO A FILE
-    # file1 = open(sequence_file, 'w')
-    for state in decoded_states:
-        label = state_labels[state]
-        # file1.write(f"{label}, ")
-        sequence.append(label)
-    # file1.close()        
-
-    return decoded_states
-
-def plot_snps(df):
-    groups = df.groupby('z')
-    for name, group in groups:
-        plt.plot(group.x, group.y, marker='o', linestyle='', markersize=0.5, label=name)
-    
-    plt.ylim([0, 1])
-    plt.xlabel("SNP Position")
-    plt.ylabel("BAF")
-    plt.legend(loc='upper left', bbox_to_anchor=(1.04, 1))
-
-    plt.show()
-
-def filter_snps(vcf_file, outfile, purity):
+def filter_snps(vcf_file, outfile, hmm_file):
     """
     in order to preserve original VCF formatting as much as possible to avoid downstream errors with other programs
     """
@@ -213,42 +137,13 @@ def filter_snps(vcf_file, outfile, purity):
     AD = het_df_snp['AD'].to_numpy()
     DP = het_df_snp['DP'].to_numpy()
 
-    # pass in AD, DP, purity, start/trans probabilities into hmm function
-    decoded_states = hmm_sequence(AD, DP, purity)
-
-    # find indices where decoded_states is "normal"
-    normal_indices = np.where(decoded_states == 1)[0]
-    AD_norm = np.array(AD)[normal_indices]
-    DP_norm = np.array(DP)[normal_indices]
-    norm_count = normal_indices.size
-
-    # initialize counts (for percentages)
-    count_00 = 0
-    count_11 = 0
-
-    # for each "normal" het SNP, change decoded state to 2 if p_val < 0.05
-    # these states are likely falsely-labeled het SNPs
-    for i in range(norm_count):
-        p_value = scipy.stats.binomtest(AD_norm[i], n=DP_norm[i], p=0.5, alternative='two-sided').pvalue
-        curr_BAF = AD_norm[i]/DP_norm[i]
-        if p_value < 0.05:
-            decoded_states[normal_indices[i]] = 2
-            if curr_BAF < 0.5:
-                count_00 += 1
-            else:
-                count_11 += 1
+    # pickle het_df_snp
+    with open(hmm_file, 'wb') as file:
+        pickle.dump(het_df_snp, file)
+        file.close()
     
-    # report percentage of Normal hetSNPs that are marked as homo ref vs. homo alt
-    percentage_00 = (count_00 / norm_count) * 100
-    percentage_11 = (count_11 / norm_count) * 100   
-    print(f"Normal -> 0/0: {percentage_00}%")
-    print(f"Normal -> 1/1: {percentage_11}%")
-
-    # plots het SNP BAF values
-    het_df = pd.DataFrame({'x': het_df_snp["POS"].to_numpy(),
-                           'y': np.divide(AD, DP),
-                           'z': decoded_states})
-    plot_snps(het_df)
+    # for now, exit(0) because we don't want to print out the genotypes just yet
+    exit(0)
 
     hi_depth_positions = set(zip(hi_depth.CHROM.astype('str'), hi_depth.POS.astype('str')))
     to_print = []
@@ -269,8 +164,8 @@ def filter_snps(vcf_file, outfile, purity):
     with gzip.open(outfile, 'wt') as o:
         o.write("\n".join(to_print))
 
-def main(infile, outfile, purity):
-    filter_snps(infile, outfile, purity)
+def main(infile, outfile, hmm_file):
+    filter_snps(infile, outfile, hmm_file)
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2], sys.argv[3])
