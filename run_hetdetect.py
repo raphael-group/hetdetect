21fe91fe15d6384e014e747ed46d1dfc8d180a5d
Metin Balaban <mebalaba@eng.ucsd.edu>
2023-07-12 00:45:58 -0400

    setup file and compressed input/output

--- run_hetdetect.py
+++ run_hetdetect.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import logging
 import argparse
 import os 
@@ -11,6 +10,12 @@ from os.path import join
 import numpy as np
 import scipy
 
+def compress_output_decision():
+    if options.compress:
+        logging.info(f"Compressing output VCF files")
+        os.system(f"bgzip -f {join(options.output_fp, 'hetdetect.vcf')}")
+        for k in bychrom_writers.keys():
+            os.system(f"bgzip -f {join(options.output_fp, 'bychrom', f'{k}.vcf')}")
 
 if __name__ == "__main__":
     root = logging.getLogger()
@@ -27,6 +32,8 @@ if __name__ == "__main__":
                                   metavar="DIRECTORY")
     parser.add_argument("--nohmm", dest="nohmm", action='store_true', default=False,
                                   help="turn off hmm genotyping")
+    parser.add_argument("-c", "--compress", dest="compress", action='store_true', default=False,
+                                  help="output compressed VCF files")
     parser.add_argument("-f", "--dpfilter", type=int, dest="dp_filter", default=0,
                                   help="minimum sequencing depth (DP) needed to include in the output VCF file(s)"
                                        , metavar="NUMBER")
@@ -42,66 +49,66 @@ if __name__ == "__main__":
             print("hetdetect version " + __version__, flush=True)
             exit(0)                           
 
-    with open(options.input_fp, "r") as input_fp:
-        vcf_reader = vcf.Reader(input_fp)
 
-        if len(vcf_reader.samples) <= 0:
-            logging.info("Input VCF does not have existing genotyping. We are taking DP and AD values from INFO field and start calling")
-            vcf_reader._column_headers += ["FORMAT", "sample"] 
-        elif len(vcf_reader.samples) > 1:
-            logging.ERROR("We currently only support single sample VCF files")
-            exit(1)
-        else:
-            pass # proceed with execution
-        
-        #output_fp = open(join(options.output_fp, "hetdetect.vcf"),"w")
-        output_main_nohmm_fp = open(join(options.output_fp, "hetdetect.nohmm.vcf"),"w")
-        vcf_writer = vcf.Writer(output_main_nohmm_fp, vcf_reader)
-        if not os.path.exists(join(options.output_fp, "bychrom")):
-            os.mkdir(join(options.output_fp, "bychrom"))
-        bychrom_writers = dict()
+    vcf_reader = vcf.Reader(filename=options.input_fp)
 
-        i = 0
-        logging.info("Re-genotyping without HMM")
-        #re-genotyping AD > 0 & DP-AD > 0 as 0/1
-        for record in vcf_reader:
-            if len(record.samples) == 0:
-                # converting from cellsnplite format to bcftools format temporarily
-                CallData = vcf.model.make_calldata_tuple(["GT","DP","AD"])
-                DP = int(record.INFO["DP"][0])
-                AD = int(record.INFO["AD"][0])
-                REF = DP - AD
-                record.samples = [vcf.model._Call(record,
-                                                  sample="sample",
-                                                  data=CallData("0/0", DP=DP, AD=[REF, AD]))] # 0/0 is a placeholder
-                record.FORMAT = "GT:DP:AD"
-            sample = record.samples[0]
-            REF = sample.data.AD[0]
-            if sample.data.DP < options.dp_filter:
-                 continue
-            if len(sample.data.AD) > 1:
-                 ALT = sample.data.AD[1]
-            else:
-                 ALT = 0
+    if len(vcf_reader.samples) <= 0:
+        logging.info("Input VCF does not have existing genotyping. We are taking DP and AD values from INFO field and start calling")
+        vcf_reader._column_headers += ["FORMAT", "sample"] 
+    elif len(vcf_reader.samples) > 1:
+        logging.ERROR("We currently only support single sample VCF files")
+        exit(1)
+    else:
+        pass # proceed with execution
+    
+    #output_fp = open(join(options.output_fp, "hetdetect.vcf"),"w")
+    output_main_nohmm_fp = open(join(options.output_fp, "hetdetect.nohmm.vcf"),"w")
+    vcf_writer = vcf.Writer(output_main_nohmm_fp, vcf_reader)
+    if not os.path.exists(join(options.output_fp, "bychrom")):
+        os.mkdir(join(options.output_fp, "bychrom"))
+    bychrom_writers = dict()
 
-            if ALT > 0 and REF > 0:
-                GT = "0/1"
-            elif ALT > 0 and REF == 0:
-                GT = "1/1"
-            else:
-                GT = "0/0"
-            
-            # overwrite the Calldata with the new genotype
+    i = 0
+    logging.info("Re-genotyping without HMM")
+    #re-genotyping AD > 0 & DP-AD > 0 as 0/1
+    for record in vcf_reader:
+        if len(record.samples) == 0:
+            # converting from cellsnplite format to bcftools format temporarily
             CallData = vcf.model.make_calldata_tuple(["GT","DP","AD"])
-            # using cellsnplite format where AD is the alternate allele count only
-            record.samples[0].data = CallData(GT,DP=sample.data.DP, AD=ALT)
+            DP = int(record.INFO["DP"][0])
+            AD = int(record.INFO["AD"][0])
+            REF = DP - AD
+            record.samples = [vcf.model._Call(record,
+                                                sample="sample",
+                                                data=CallData("0/0", DP=DP, AD=[REF, AD]))] # 0/0 is a placeholder
             record.FORMAT = "GT:DP:AD"
-            vcf_writer.write_record(record)
-            # In addition to the main vcf output file, we write each chromosome separately to individual files
-            if record.CHROM not in bychrom_writers:
-                    bc_fp = open(join(options.output_fp, "bychrom",f"{record.CHROM}.nohmm.vcf"),"w")
-                    bychrom_writers[record.CHROM] = vcf.Writer(bc_fp, vcf_reader)
-            bychrom_writers[record.CHROM].write_record(record)
+        sample = record.samples[0]
+        REF = sample.data.AD[0]
+        if sample.data.DP < options.dp_filter:
+                continue
+        if len(sample.data.AD) > 1:
+                ALT = sample.data.AD[1]
+        else:
+                ALT = 0
+
+        if ALT > 0 and REF > 0:
+            GT = "0/1"
+        elif ALT > 0 and REF == 0:
+            GT = "1/1"
+        else:
+            GT = "0/0"
+        
+        # overwrite the Calldata with the new genotype
+        CallData = vcf.model.make_calldata_tuple(["GT","DP","AD"])
+        # using cellsnplite format where AD is the alternate allele count only
+        record.samples[0].data = CallData(GT,DP=sample.data.DP, AD=ALT)
+        record.FORMAT = "GT:DP:AD"
+        vcf_writer.write_record(record)
+        # In addition to the main vcf output file, we write each chromosome separately to individual files
+        if record.CHROM not in bychrom_writers:
+                bc_fp = open(join(options.output_fp, "bychrom",f"{record.CHROM}.nohmm.vcf"),"w")
+                bychrom_writers[record.CHROM] = vcf.Writer(bc_fp, vcf_reader)
+        bychrom_writers[record.CHROM].write_record(record)
     
     vcf_writer.close()
     for k,v in bychrom_writers.items():
@@ -115,6 +122,7 @@ if __name__ == "__main__":
         for k, _ in bychrom_writers.items():
             shutil.move(join(options.output_fp, "bychrom",f"{k}.nohmm.vcf"),
                         join(options.output_fp, "bychrom",f"{k}.vcf"))
+        compress_output_decision()
         exit(0)
 
     def hmm_genotyper(k):
@@ -201,6 +209,8 @@ if __name__ == "__main__":
                 for record in vcf_reader:
                     vcf_writer.write_record(record)
         vcf_writer.close()
+    logging.info(f"Done writing the output VCF file")
+    compress_output_decision()
     os.remove(join(options.output_fp, "hetdetect.nohmm.vcf"))
     
     
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+exec(open('hetdetect/version.py').read())
+setup(
+        name='hetdetect',    # This is the name of your PyPI-package.
+        version=__version__,    # Update the version number for new releases
+        # The name of your script, and also the command you'll be using for calling it
+        # Also other executables needed
+        scripts=['run_hetdetect.py',],
+        description='Heterozygous SNP detection in tumor samples',
+        long_description='Heterozygous SNP detection in tumor samples using a Hidden Markov Model. Supports CPU and GPU.',
+        long_description_content_type='text/plain',
+        url='https://github.com/raphael-group/tumor-hetsnp-inference',
+        author='Melody Choi & Metin Balaban', 
+        author_email='metin@princeton.edu',
+        packages=find_packages(),
+        zip_safe = False,
+        install_requires=['matplotlib','pandas','pyvcf3','hmmlearn','pomegranate','bgzip'],
+        include_package_data=True
+)
