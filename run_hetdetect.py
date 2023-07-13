1e03c6b8bd40bd30a3b3ad405ad1bd06ab624fa6
Metin Balaban <mebalaba@eng.ucsd.edu>
2023-07-12 22:39:07 -0400

    remove old code

--- run_hetdetect.py
+++ run_hetdetect.py
@@ -144,16 +144,6 @@ if __name__ == "__main__":
             sample=f"0/0:{DP}:{ADstring}"
             # converting from cellsnplite format to bcftools format temporarily
             record = vcf_writer.variant_from_string(f"{CHROM}\t{POS}\t{ID}\t{REF}\t{ALT}\t{QUAL}\t{FILTER}\tDP={DP};AD={ADstring};OTH={OTH}\t{FORMAT}\t{sample}")
-            # record.FORMAT = ["GT","DP","AD"]
-            # record.genotypes = np.array([[0,0,False]])
-            # CallData = vcf.model.make_calldata_tuple(["GT","DP","AD"])
-            # DP = int(record.INFO["DP"][0])
-            # AD = int(record.INFO["AD"][0])
-            # REF = DP - AD
-            # record.samples = [vcf.model._Call(record,
-            #                                     sample="sample",
-            #                                     data=CallData("0/0", DP=DP, AD=[REF, AD]))] # 0/0 is a placeholder
-            # record.FORMAT = "GT:DP:AD"
 
         if type(record.INFO.get("AD")) == int:
             write_record(record)           
@@ -166,30 +156,6 @@ if __name__ == "__main__":
         else:
             record.genotypes = np.array([[0,0,False]])
         write_record(record)
-        # sample = record.samples[0]
-        # REF = sample.data.AD[0]
-        # if sample.data.DP < options.dp_filter:
-        #         continue
-        # if len(sample.data.AD) > 1:
-        #         ALT = sample.data.AD[1]
-        # else:
-        #         ALT = 0
-
-        # if ALT > 0 and REF > 0:
-        #     GT = "0/1"
-        # elif ALT > 0 and REF == 0:
-        #     GT = "1/1"
-        # else:
-        #     GT = "0/0"
-        
-        # # overwrite the Calldata with the new genotype
-        # CallData = vcf.model.make_calldata_tuple(["GT","DP","AD"])
-        # # using cellsnplite format where AD is the alternate allele count only
-        # record.samples[0].data = CallData(GT,DP=sample.data.DP, AD=ALT)
-        # record.FORMAT = "GT:DP:AD"
-        # vcf_writer.write_record(record)
-
-        # bychrom_writers[record.CHROM].write_record(record)
 
     vcf_writer.close()
     for k,v in bychrom_writers.items():
@@ -258,10 +224,6 @@ if __name__ == "__main__":
                 index = (record.CHROM, record.POS)
                 if index in newGTdict:
                     record.genotypes = np.array([newGTdict[index]])
-                    # sample = record.samples[0]
-                    # CallData = vcf.model.make_calldata_tuple(["GT","DP","AD"])
-                    # record.samples[0].data = CallData(newGTdict[index],DP=sample.data.DP, AD=sample.data.AD)
-                    # record.FORMAT = "GT:DP:AD"
                 vcf_writer.write_record(record)
             vcf_writer.close()
         logging.info(f"New HMM genotypes are written for chromosome {k}")
